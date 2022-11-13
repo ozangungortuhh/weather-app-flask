11f377ad8e8b9a34c96a10329980e95169bccf56
o.guengoer <o.guengoer@synergeticon.de>
2022-11-13 17:21:33 +0100

    Add db with 3 cities

--- app.py
+++ app.py
@@ -4,21 +4,33 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
 
-API_KEY = '932ad200713d0d59dcafdea4a1efa141'
+# Create db
+db = SQLAlchemy(app)
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
 
 @app.route('/')
 def index():
-    city = 'Hamburg'
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={API_KEY}'
+    cities = City.query.all()
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=932ad200713d0d59dcafdea4a1efa141'
     
-    r = requests.get(url.format(city)).json()
-    print(r)
+    weather_data = list()
 
-    weather = {
-        'city' : city,
-        'temperature' : r['main']['temp'] ,
-        'description' : r['weather'][0]['description'] ,
-        'icon' : r[weather][0]['icon'],
-    }
-    return render_template('weather.html')
+    for city in cities:
+        print(city.name)
+
+        r = requests.get(url.format(city.name)).json()
+        weather = {
+            'city' : city.name,
+            'temperature' : r['main']['temp'],
+            'description' : r['weather'][0]['description'],
+            'icon' : r['weather'][0]['icon'],
+        }
+
+        weather_data.append(weather)
+
+    return render_template('weather.html', weather_data=weather_data)
